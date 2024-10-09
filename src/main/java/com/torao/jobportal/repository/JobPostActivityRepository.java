3c0f5a2961853386e98da678e323c86867f5b228
Torao <bahoann79@gmail.com>
2024-10-09 14:33:58 +0700

    feat: add job post function for recruiter

---
+++ src/main/java/com/torao/jobportal/repository/JobPostActivityRepository.java
@@ -0,0 +1,7 @@
+package com.torao.jobportal.repository;
+
+import com.torao.jobportal.entity.JobPostActivity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JobPostActivityRepository extends JpaRepository<JobPostActivity, Integer> {
+}
---
+++ src/main/java/com/torao/jobportal/service/JobPostActivityService.java
@@ -0,0 +1,22 @@
+package com.torao.jobportal.service;
+
+import com.torao.jobportal.entity.JobPostActivity;
+import com.torao.jobportal.repository.JobPostActivityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JobPostActivityService {
+
+    private final JobPostActivityRepository jobPostActivityRepository;
+
+    @Autowired
+    public JobPostActivityService(JobPostActivityRepository jobPostActivityRepository) {
+        this.jobPostActivityRepository = jobPostActivityRepository;
+    }
+
+    public JobPostActivity addNew(JobPostActivity jobPostActivity){
+        return jobPostActivityRepository.save(jobPostActivity);
+    }
+
+}
